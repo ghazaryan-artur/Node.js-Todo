42fc546a0a9adf753ed5fb0a89d89f278c2a676f
Артур Казарян <arturkazaran@Arturs-MacBook-Pro.local>
2020-10-22 05:13:03 +0400

    low bug fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require('body-parser');
 const path = require('path');
 const todoRouter = require('./routes/todos.router');
 const { errorHandler }  = require("./middleware/errorHandler");
